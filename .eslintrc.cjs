27dafe67ad6140a82d9d23342d6e95b1cbc6f446
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-06-27 23:20:16 +0200

    installed and configured eslint with prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 export default function App() {
   return <div>App</div>
 }
