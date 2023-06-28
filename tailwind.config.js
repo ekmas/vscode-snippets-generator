c9a6fb5a0cfc2c50326de958dff7c2e63cd9a6f8
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-06-28 22:51:44 +0200

    added css variables and changed body styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        container: '1300px',
+      },
+      padding: {
+        container: '20px 30px'
+      },
+      colors: {
+        mainBg: '#2c2c32',
+        secondaryBg: '#25252b',
+        primaryBlue: '#0066b8',
+        secondaryBlue: '#005ba4',
+        lightGray: '#cccccc',
+        gray: '#6e6e6e',
+        darkGray: '#333333',
+        darkerGray: '#242424'
+      }
+    },
   },
   plugins: [],
 }
