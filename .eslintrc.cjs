ac5f9836b4cf3a840ae396ff7a86a4773036ec3b
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-06-28 22:51:03 +0200

    fixed issues with eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,5 +29,11 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 }
