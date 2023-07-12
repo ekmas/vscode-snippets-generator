83b8ed7fdb4e09684c41130524eb35d221804c1c
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-12 22:00:09 +0200

    added Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Main from './components/Main'
 
 export default function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+import AddSnippet from './AddSnippet'
+
+export default function Main() {
+  return (
+    <div className="w-[1300px] mx-auto gap-10 px-5 grid grid-cols-2">
+      <AddSnippet />
+    </div>
+  )
+}
