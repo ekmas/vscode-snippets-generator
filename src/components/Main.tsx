31c94fad740ca472718e78b54634b6bbad4eafbc
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-15 00:24:21 +0200

    added link to snippet docs

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,10 +7,20 @@ export default function Main() {
   const [snippets, setSnippets] = useState<Snippet[]>([])
 
   return (
-    <div className="min-h-[calc(100vh-225px-112px)] flex justify-center items-center">
-      <div className="w-[1300px] mx-auto gap-10 px-5 grid grid-cols-2 h-full">
+    <div className="w-[1300px] mx-auto min-h-[calc(100vh-225px-112px)] flex flex-col justify-center items-center">
+      <div className="w-full gap-10 px-5 grid grid-cols-2 h-full">
         <AddSnippet setSnippets={setSnippets} />
         <CopySnippets snippets={snippets} />
+      </div>
+      <div className="py-4">
+        <a
+          target="_blank"
+          className="text-primaryBlue font-medium underline"
+          href="https://code.visualstudio.com/docs/editor/userdefinedsnippets"
+          rel="noreferrer"
+        >
+          VSCode Snippet Docs
+        </a>
       </div>
     </div>
   )
