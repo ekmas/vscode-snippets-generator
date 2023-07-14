7f9f523c035db00fee3d8eebf288b03e43780a06
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-14 23:26:38 +0200

    added CopyModal

--- src/components/CopyBtn.tsx
+++ src/components/CopyBtn.tsx
@@ -1,30 +1,48 @@
+import { useState } from 'react'
+import CopyModal from './CopyModal'
+
 type Props = {
   noSnippetsYet: boolean
   editorValue: string
 }
 
 export default function CopyBtn({ noSnippetsYet, editorValue }: Props) {
+  const [isModalActive, setIsModalActive] = useState(false)
+  const [isCopySuccessful, setIsCopySuccessful] = useState<boolean | null>(
+    false
+  )
+
   const handleCopySnippets = () => {
     navigator.clipboard
       .writeText(editorValue)
       .then(() => {
-        console.log('Value copied to clipboard')
+        setIsCopySuccessful(false)
       })
       .catch((error) => {
-        console.error('Failed to copy value:', error)
+        setIsCopySuccessful(false)
       })
+
+    setIsModalActive(true)
   }
 
   return (
-    <button
-      onClick={handleCopySnippets}
-      className={
-        noSnippetsYet
-          ? 'bg-gray cursor-not-allowed transition-all mt-3 text-white w-full px-5 py-2'
-          : 'bg-primaryBlue transition-all mt-3 text-white w-full hover:bg-secondaryBlue px-5 py-2'
-      }
-    >
-      Copy snippets
-    </button>
+    <>
+      <button
+        onClick={handleCopySnippets}
+        disabled={noSnippetsYet}
+        className={
+          noSnippetsYet
+            ? 'bg-gray cursor-not-allowed transition-all mt-3 text-white w-full px-5 py-2'
+            : 'bg-primaryBlue transition-all mt-3 text-white w-full hover:bg-secondaryBlue px-5 py-2'
+        }
+      >
+        Copy snippets
+      </button>
+      <CopyModal
+        active={isModalActive}
+        setIsActive={setIsModalActive}
+        isCopySuccesful={isCopySuccessful}
+      />
+    </>
   )
 }
---
+++ src/components/CopyModal.tsx
@@ -0,0 +1,57 @@
+type Props = {
+  active: boolean
+  setIsActive: (active: boolean) => void
+  isCopySuccesful: boolean | null
+}
+
+export default function CopyModal({
+  active,
+  setIsActive,
+  isCopySuccesful,
+}: Props) {
+  return (
+    <div
+      className={
+        active
+          ? 'z-20 fixed top-0 left-0 w-screen h-full flex items-center justify-center backdrop-blur-sm transition-all opacity-100 visible'
+          : 'z-20 fixed top-0 left-0 w-screen h-full flex items-center justify-center transition-all opacity-0 invisible'
+      }
+    >
+      <div className="w-[550px] h-min bg-editorBg text-center p-5 text-white border border-white/50">
+        <h2 className="text-2xl">
+          {isCopySuccesful
+            ? 'Snippets have been copied.'
+            : "Your device doesn't support navigator api."}
+        </h2>
+
+        {!isCopySuccesful && (
+          <p className="mt-6 leading-[1.8] text-[15px]">
+            You&quot;ll have to click on the editor on the right with snippets,
+            select them all and copy them that way.
+          </p>
+        )}
+
+        <p className="my-6 leading-[1.8] text-[15px]">
+          Now open VSCode and open command palette with <br />{' '}
+          <b>Ctrl + Shift + P</b>. <br /> Search for &quot;Snippets: Configure
+          User Snippets&quot; <br />
+          Select snippet file for desired language (if desired language is not
+          on the list either download extension for that language or place it in
+          global snippet file) <br />
+          After you opened snippet file, just delete everything and paste your
+          snippets.
+        </p>
+
+        <button
+          type="button"
+          onClick={() => {
+            setIsActive(false)
+          }}
+          className="bg-primaryBlue hover:bg-secondaryBlue transition-colors px-5 py-1.5"
+        >
+          Ok
+        </button>
+      </div>
+    </div>
+  )
+}
