a0fb5c21c127d4b14ba6dd6b595962ed8b25541d
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-14 21:25:24 +0200

    changed heading thickness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,9 @@ export default function Header() {
         <div className="flex flex-col items-center mx-auto w-max py-5">
           <img src={logo} alt="vscode logo" width={80} />
 
-          <h1 className="text-white text-[30px] mt-5">Snippets generator</h1>
+          <h1 className="text-white text-[30px] mt-5 font-thin">
+            Snippets generator
+          </h1>
         </div>
       </div>
     </header>
