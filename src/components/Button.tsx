7341107b1daa5e281a2c8a5313dd6437a68cc4c2
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-12-11 22:47:31 +0100

    added Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+import { cn } from '../lib/utils'
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center transition-all text-white whitespace-nowrap',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primaryBlue hover:bg-secondaryBlue',
+      },
+      size: {
+        default: 'px-5 py-2',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
